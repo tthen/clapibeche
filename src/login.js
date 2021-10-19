89388c213ba0faa0b018fe0018d7e560035478cd
tthen <85257109+tthen@users.noreply.github.com>
2021-10-19 17:33:21 -0500

    Update login.js

--- src/login.js
+++ src/login.js
@@ -1,13 +1,5 @@
 // let registros = [];
-
-let registros = [
-    { correo: "juan@gmail.com", contrasena: "SecurePassword123" },
-    { correo: "juan@gmail.com", contrasena: "SecurePassword123" },
-    { correo: "juan@gmail.com", contrasena: "SecurePassword123" },
-    { correo: "juan@gmail.com", contrasena: "SecurePassword123" },
-    { correo: "juan@gmail.com", contrasena: "SecurePassword123" },
-    { correo: "juan@gmail.com", contrasena: "SecurePassword123" },
-];
+let registros = [{correo: "juan@gmail.com", contrasena: "SecurePassword123"}];
 
 function agregarRegistro() {
 
@@ -27,41 +19,31 @@ function agregarRegistro() {
     registros.push(regs);
 }
 
-
-function login (){
-    var correo = document.getElementById('correo').value;
-    var contrasena = document.getElementById('contrasena').value;
-    var captcha = document.getElementById('captcha').value;
-
-    for (i in registros) {
-        if ( i.correo === correo && i.contrasena === contrasena && validarCAPTCHA(Number(captcha)) === true ) {
-                return true;
-        } else {
-            return false;
-        }
+function login(){
+    const fCorreo = document.getElementById("correo").value; 
+    const fContrasena = document.getElementById("contrasena").value;
+    const fCaptcha = document.getElementById('captcha').value;
+    let trueCorreo = registros.find(e => e.correo == fCorreo);
+    let trueContrasena = registros.find(e => e.contrasena == fContrasena);
+    if( trueCorreo  && trueContrasena  && validarCAPTCHA(fCaptcha)){
+       return true;
+    } else { 
+	   return false;
     }
+    
+    
+} 
+
+function validarCAPTCHA(valor){
+	if (valor==1000){
+		return true;
+	}
+	else{
+		return false;
+	}   
 }
-
-
-// function prueba () {
-//     if ( correo === "henrytorres@mail.com" && contrasena === "123" && validarCAPTCHA((captcha)) === true ) {
-//             return true;
-//     } else {
-//         return false;
-//     }
-// }
-
-
-function validarCAPTCHA (valor) {
-    if (valor === 1000) {
-        return true;
-      } else {
-        return false;
-      }
-}
-
-
-module.exports.registros = registros;
-module.exports.agregarRegistro = agregarRegistro;
-module.exports.validarCAPTCHA = validarCAPTCHA;
+    
 module.exports.login = login;
+module.exports.registros = registros;
+module.exports.validarCAPTCHA = validarCAPTCHA;
+module.exports.agregarRegistro = agregarRegistro;
